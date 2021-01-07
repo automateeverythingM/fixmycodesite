dc0cbe70fbb274bc51811d195466608c3ed66928
automateeverythingM <markospavicbg@gmail.com>
2021-01-07 20:13:37 +0100

    fix on hover on navLink

--- src/components/Layout/UserOptionDropDown/NotifyIcon.js
+++ src/components/Layout/UserOptionDropDown/NotifyIcon.js
@@ -6,24 +6,32 @@ import { Badge } from "react-bootstrap";
 import { FaBell } from "react-icons/fa";
 
 const NotifyIcon = ({
-  color = "white",
+  color = "#ffffff88",
   size = "1rem",
   notificationNumber = 2,
 }) => {
   return (
-    <span className=" text-white position-relative">
+    <span
+      className="position-relative"
+      css={css`
+        color: #ffffffaa;
+        &:hover {
+          color: #fff;
+        }
+      `}
+    >
       {notificationNumber && (
         <Badge
           css={css`
             top: -0.5rem;
             right: -0.2rem;
           `}
-          className="bg-danger position-absolute"
+          className="text-white bg-danger position-absolute"
         >
           {notificationNumber}
         </Badge>
       )}
-      <FaBell size={size} color={color} />
+      <FaBell size={size} />
     </span>
   );
 };
